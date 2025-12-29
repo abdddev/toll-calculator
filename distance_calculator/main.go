b7cc794e340de8aaa987fb63c5481d4d4d83d4ac
abdddev <abdddev@gmail.com>
2025-12-29 20:46:55 +1000

    implement distance calculator service

---
+++ distance_calculator/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+)
+
+const (
+	kafkaTopic         = "obudata"
+	aggregatorEndpoint = "http://127.0.0.1:3000"
+)
+
+func main() {
+	var (
+		err error
+		svc CalculatorServicer
+	)
+	svc = NewCalculatorService()
+	//svc = NewLogMiddleware(svc)
+
+	//httpClient := client.NewHTTPClient(aggregatorEndpoint)
+	// grpcClient, err := client.NewGRPCClient(aggregatorEndpoint)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	kafkaConsumer, err := NewKafkaConsumer(kafkaTopic, svc)
+	if err != nil {
+		log.Fatal(err)
+	}
+	kafkaConsumer.Start()
+}
